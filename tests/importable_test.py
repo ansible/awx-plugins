5284999392779623049b09e3eac40fb9870ef289
Chris Meyers <chris.meyers.fsu@gmail.com>
2024-08-13 15:52:39 -0400

    Init cred and inv plugins

--- tests/importable_test.py
+++ tests/importable_test.py
@@ -10,7 +10,6 @@ import pytest
 @pytest.mark.parametrize(
     'entry_points_group',
     (
-        'awx.credential_plugins',
         'awx_plugins.credentials',
     ),
 )
@@ -21,17 +20,31 @@ def test_entry_points_exposed(entry_points_group: str) -> None:
     pre-installed.
     """
     entry_points = _discover_entry_points(group=entry_points_group)
-    assert 'x' in entry_points.names
+    expected_entry_points = {
+        'aim': 'aim_plugin',
+        'conjur': 'conjur_plugin',
+        'hashivault_kv': 'hashivault_kv_plugin',
+        'hashivault_ssh': 'hashivault_ssh_plugin',
+        'azure_kv': 'azure_keyvault_plugin',
+        'centrify_vault_kv': 'centrify_plugin',
+        'thycotic_dsv': 'dsv_plugin',
+        'thycotic_tss': 'tss_plugin',
+        'aws_secretsmanager_credential': 'aws_secretmanager_plugin',
+    }
+    for x in expected_entry_points.keys():
+        assert x in entry_points.names
 
-    callable_ref_spec = entry_points['x'].value
-    import_ref, callable_sep, callable_ref = callable_ref_spec.partition(':')
-    assert callable_sep
+    for k, v in expected_entry_points.items():
+        callable_ref_spec = entry_points[k].value
+        import_ref, callable_sep, callable_ref = callable_ref_spec.partition(':')
+        assert callable_sep
+        assert v == str(callable_ref)
 
-    test_is_importable_callable_cmd = (
-        _current_runtime,
-        '-c',
-        f'from sys import exit; '
-        f'from {import_ref} import {callable_ref}; '
-        f'exit(not callable({callable_ref}))',
-    )
-    _invoke_command(test_is_importable_callable_cmd)
+        test_is_importable_callable_cmd = (
+            _current_runtime,
+            '-c',
+            f'from sys import exit; '
+            f'from {import_ref} import {callable_ref}; '
+            f'exit(not type({callable_ref}).__name__ == "CredentialPlugin")',
+        )
+        _invoke_command(test_is_importable_callable_cmd)
