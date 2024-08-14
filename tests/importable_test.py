8e7178e906e84a05fbf6a0efdbf06be5ec21df1b
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2024-08-14 16:05:26 +0200

    Move the entire smoke test to the same process

--- tests/importable_test.py
+++ tests/importable_test.py
@@ -1,8 +1,6 @@
 """Smoke tests related to loading entry points."""
 
 from importlib.metadata import entry_points as _discover_entry_points
-from subprocess import check_call as _invoke_command
-from sys import executable as _current_runtime
 
 import pytest
 
@@ -25,11 +23,4 @@ def test_entry_points_exposed(entry_points_group: str) -> None:
 
     assert entry_points['x'].value == 'awx_plugins.credentials.x.api:XPlugin'
 
-    test_is_importable_callable_cmd = (
-        _current_runtime,
-        '-c',
-        'from importlib.metadata import entry_points; '
-        'from sys import exit; '
-        f'exit(not callable(entry_points(group="{entry_points_group}")["x"].load()))',
-    )
-    _invoke_command(test_is_importable_callable_cmd)
+    assert callable(entry_points['x'].load())
