da1cf6c355fc494c280e7ab3994f6ee9cfc4ac0c
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2024-08-18 04:11:59 +0200

    docs integration

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,25 +1,33 @@
 # How to Contribute
 
-This project is [GPL-3.0 licensed](COPYING) and accepts contributions through
+This project is [Apache-2.0 licensed] and accepts contributions through
 GitHub pull requests.
+
+[Apache-2.0 licensed]:
+https://github.com/ansible/awx-plugins/blob/devel/LICENSE
 
 ## Certificate of Origin
 
 By contributing to this project you agree to the Developer Certificate of
 Origin (DCO). This document was created by the Linux Kernel community and is a
 simple statement that you, as a contributor, have the legal right to make the
-contribution. See the [DCO](DCO) file for details.
+contribution. See the [DCO] file for details.
+
+[DCO]: https://github.com/ansible/awx-plugins/blob/devel/DCO
 
 ## Principles
 
 This repository adheres to the following principles:
 
 - Open: Contribution is always welcome.
-- Respectful: See the [Code of Conduct](CODE_OF_CONDUCT.md).
+- Respectful: See the [Code of Conduct].
 - Transparent and accessible: Work and collaboration should be done in public.
   See [Governance](#governance) section for details.
 - Merit: Ideas and contributions are accepted according to their merit and
   alignment with the project objectives principles.
+
+[Code of Conduct]:
+https://github.com/ansible/awx-plugins/blob/devel/CODE-OF-CONDUCT.md
 
 ## How to contribute
 
@@ -27,8 +35,8 @@ We are very happy to receive contributions from the community in any form!
 
 Please use a GitHub pull request to submit your contributions. If you have a
 question or are unsure if a contribution is wanted, please join us in
-[TBD](#channel-name-here) on Matrix to discuss your change or on the Ansible forum
-using the TBD tag if you prefer async discussion.
+<!--TBD-->#channel-name-here on Matrix to discuss your change or on the Ansible
+forum using the TBD tag if you prefer async discussion.
 Open a GitHub issue to report bugs or request features.
 
 ## Governance
---
+++ docs/_ext/spelling_stub_ext.py
@@ -0,0 +1,73 @@
+"""Sphinx extension for making the spelling directive noop."""
+
+from typing import List
+
+from sphinx.application import Sphinx
+from sphinx.config import Config as _SphinxConfig
+from sphinx.util import logging
+from sphinx.util.docutils import SphinxDirective
+from sphinx.util.nodes import nodes
+
+try:
+    from enchant.tokenize import (  # noqa: WPS433
+        Filter as _EnchantTokenizeFilterBase,
+    )
+except ImportError:
+    _EnchantTokenizeFilterBase = object  # noqa: WPS440
+
+
+logger = logging.getLogger(__name__)
+
+
+def _configure_spelling_ext(app: Sphinx, config: _SphinxConfig) -> None:
+    class VersionFilter(_EnchantTokenizeFilterBase):  # noqa: WPS431
+        # NOTE: It's nested because we need to reference the config by closure.
+        """Filter for treating version words as known."""
+
+        def _skip(self, word: str) -> bool:
+            # NOTE: Only accessing the config values in the method since they
+            # NOTE: aren't yet populated when the config-inited event happens.
+            known_version_words = {
+                config.release,
+                config.version,
+            }
+            if word not in known_version_words:
+                return False
+
+            logger.debug(
+                'Known version words: %r',  # noqa: WPS323
+                known_version_words,
+            )
+            logger.debug(
+                'Ignoring %r because it is a known version',  # noqa: WPS323
+                word,
+            )
+
+            return True
+
+    app.config.spelling_filters = [VersionFilter]
+    app.setup_extension('sphinxcontrib.spelling')
+    del app.config.spelling_filters  # suppress unpicklable value warnings
+
+
+class SpellingNoOpDirective(SphinxDirective):
+    """Definition of the stub spelling directive."""
+
+    has_content = True
+
+    def run(self) -> List[nodes.Node]:
+        """Generate nothing in place of the directive."""
+        return []
+
+
+def setup(app: Sphinx) -> None:
+    """Initialize the extension."""
+    if _EnchantTokenizeFilterBase is object:
+        app.add_directive('spelling', SpellingNoOpDirective)
+    else:
+        app.connect('config-inited', _configure_spelling_ext)
+
+    return {
+        'parallel_read_safe': True,
+        'version': app.config.release,
+    }
