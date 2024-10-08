4e7cc436e049ae10dacde8a7748a0eacf82e0539
thedoubl3j <jljacks93@gmail.com>
2024-10-07 20:55:07 -0400

    rebase and include fixes from sviat

--- tests/credential_plugins_test.py
+++ tests/credential_plugins_test.py
@@ -158,11 +158,11 @@ def test_hashivault_handle_auth_not_enough_args() -> None:
         'expected',
     ),
     (
-	    (None, 'the_access_token'),
-	    ('access_key', 'the_access_key'),
-	    ('secret_key', 'the_secret_key'),
+        (None, 'the_access_token'),
+        ('access_key', 'the_access_key'),
+        ('secret_key', 'the_secret_key'),
     ),
-    )
+)
 def test_aws_assumerole_identifier(
     monkeypatch: pytest.MonkeyPatch,
     kwargs: dict, identifier_key: str, expected: dict[str, str],
