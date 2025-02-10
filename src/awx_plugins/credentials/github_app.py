c2a31a0277f20c5a38b69977c705f77d52719f85
🇺🇦 Sviatoslav Sydorenko (Святослав Сидоренко) <wk.cvs.github@sydorenko.org.ua>
2025-02-10 22:56:03 +0100

    Fix another missed GH API URL var

--- src/awx_plugins/credentials/github_app.py
+++ src/awx_plugins/credentials/github_app.py
@@ -57,7 +57,7 @@ github_app_inputs: GitHubAppInputs = {
                 'Specify the GitHub URL here in the case of an Enterprise Github. '
                 'Example: https://gh.your.org/api/v3',
             ),
-            'default': 'https://github.com',
+            'default': 'https://api.github.com',
         },
         {
             'id': 'app_id',
