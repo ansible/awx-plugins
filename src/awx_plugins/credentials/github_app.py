a392960f50fa416ffe19313adcb6fc9f804ab322
Sviatoslav Sydorenko <webknjaz@redhat.com>
2025-02-11 15:14:23 +0100

    Explain type ignore on cred plugin init

--- src/awx_plugins/credentials/github_app.py
+++ src/awx_plugins/credentials/github_app.py
@@ -157,7 +157,8 @@ def extract_github_app_install_token(
     return auth.token
 
 
-github_app_lookup = CredentialPlugin(
-    # type: ignore[misc]
-    'GitHub App Installation Access Token Lookup', inputs=github_app_inputs, backend=extract_github_app_install_token,
+github_app_lookup = CredentialPlugin(  # type: ignore[misc]  # untyped tuple
+    'GitHub App Installation Access Token Lookup',
+    inputs=github_app_inputs,
+    backend=extract_github_app_install_token,
 )
