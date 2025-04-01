4d14c3879fca012a053ddbfac259f72bc27bb695
Travis Neal <travis.neal@nielsen.com>
2025-04-01 09:45:57 -0700

    Add in the Cache-Control header to ensure latest credentials every time.

--- src/awx_plugins/credentials/conjur.py
+++ src/awx_plugins/credentials/conjur.py
@@ -140,6 +140,7 @@ def conjur_backend(**kwargs):
                     token.encode('utf-8'),
                 ).decode('utf-8'),
             ),
+            'Cache-Control': 'no-cache',
         },
         'allow_redirects': False,
     }
